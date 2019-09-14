10cd392807812ca6a53019caa7cb27307e139b01
ParkDongJo <pdj10211@gmail.com>
2019-09-14 10:52:04 +0900

    Add react + webpack setting code

--- react/react_practice_3/src/WordRelay.jsx
+++ react/react_practice_3/src/WordRelay.jsx
@@ -1,14 +1,44 @@
-import React, { useState, useEffect } from 'react'
+const React = require('react')
 
 const WordRelay = (props) => {
+  const [word, setWord] = React.useState('제로초')
+  const [value, setValue] = React.useState('')
+  const [result, setResult] = React.useState('')
+  const inputRef = React.useRef(null)
 
-  useEffect(() => {
+  React.useEffect(() => {
     // TODO
   })
 
-  return (
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length -1] === value[0]) {
+      setResult('딩동댕')
+      setWord(value)
+      setValue('')
+      inputRef.current.focus()
+    } else {
+      setResult('땡')
+      setValue('')
+      inputRef.current.focus()
+    }
+  }
 
+  const onChangeInput = (e) => {
+    setValue(e.target.value)
+  }
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="wordInput">input your text</label>
+        <input id="wordInput" ref={inputRef} value={value} onChange={onChangeInput} type="text"/>
+        <button>Submit</button>
+      </form>
+      <div>{result}</div>
+    </>
   )
 }
 
-module.exports = WordRelay
+module.exports = WordRelay;
