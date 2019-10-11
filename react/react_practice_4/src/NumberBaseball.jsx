9c15bfc1a380a5eba59bbdea7a73a937d70d87fc
ParkDongJo <pdj10211@gmail.com>
2019-10-11 23:30:07 +0900

    Update React key mechanism

--- react/react_practice_4/src/NumberBaseball.jsx
+++ react/react_practice_4/src/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, createRef, useState, useRef } from 'react';
 import Try from './Try';
 
 // this를 사용하지 않는다면, 밖에다 뺀다. 다른 곳에서도 사용할 수 있도록...
@@ -31,18 +31,21 @@ const NumberBaseball = () => {
   const [value, setValue] = useState('');
   const [answer, setAnswer] = useState(getNumbers());
   const [tries, setTries] = useState([]);
+  const inputEl = useRef(null);
 
   const onSubmitForm = (e) => {
     e.preventDefault();
 
     if (value === answer.join('')) {
       setResult('홈런');
+      // 함수형 
       setTries((prevTries) => [...prevTries, { try: value, result: '홈런' }]);
 
       alert('게임을 다시 시작합니다.');
       setValue('');
       setAnswer(getNumbers());
       setTries([]);
+      inputEl.current.focus();
     } else {
       const answerArr = value.split('').map((v) => parseInt(v));
 
@@ -55,6 +58,7 @@ const NumberBaseball = () => {
         setValue('');
         setAnswer(getNumbers());
         setTries([]);
+        inputEl.current.focus();
       } else {
         for (let i=0; i<4; i += 1) {
           if (answerArr[i] === answer[i]) {
@@ -65,6 +69,7 @@ const NumberBaseball = () => {
         }
         setTries((prevTries) => [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 였습니다.` }]);
         setValue('');
+        inputEl.current.focus();
       }
     }
   };
@@ -78,20 +83,92 @@ const NumberBaseball = () => {
     <>
       <h1>{result}</h1>
       <form onSubmit={onSubmitForm}>
-        <input maxLength={4} value={value} onChange={onChangeInput} />
+        <input ref={inputEl} maxLength={4} value={value} onChange={onChangeInput} />
       </form>
       <div>시도: {tries.length}</div>
       <ul>
         {
           // react 반복문에서 key는 성능 최적화를 위해 꼭 넣어줘야한다.
           // key 값은 고유한 값이어야 한다.
+          // key는 엘리먼트에 안정적인 고유성을 부여하기 위해 배열 내부의 엘리먼트에 지정해야 합니다.
           // 이때 반복문에 idx를 넣으면 key를 통한 성능 최적화가 문제가 된다.
           // react에서 key를 보고 변경된 dom을 찾고 변경시켜주는데, 이 값이 고유하지 않거나, idx를 넣어줘버리게 되면 문제가된다.
+
+          // 1. 항목들 사이에서 해당 항목을 고유하게 식별할 수 있는 문자열을 사용하는 것
+          // 2. 최후의 수단 - 안정적인 ID가 없다면 최후의 수단으로 항목의 인덱스를 key로 사용할 수 있습니다. (하지만 비추!!!)
+
+          // [ 왜 reactd에서 key에 idx를 넣지 말라는 것일까 ]
+          // 여기서 중요한 것은 각각의 item에 들어가는 key는 안정적인 고유한 값이어야 한다는것,
+          // 왜 여기서 map()쓰면서 생성되는 idx를 쓰지 말라는 걸까?
+          // 그 해답은 javascript의 내부적인 특징에 있다. javascript는 배열이 결국 링크드 리스트 구조로 되어있다.
+          // 링크드 리스트는 해당 값이 삭제되거나, 추가될 때마다 각 노드의 인덱스를 변경시켜준다.
+          // 즉 map()에서 받을수 있는 idx는 안정적이지 않고 변화가능한 값이라는 것이다.
+          // 이는 list에서 각 아이템을 제거하거나 추가하게 될때 문제가 된다.
+          // 새로운 아이템이 추가 될때 key 값이 변화가 되는 값이라면, 해당 아이템 뿐만이 아니라, 그와 연관된 값들 조차도
+          // 새롭게 생성이 된다. 이는 성능상 매우 비효율적이다.
+          // 하지만 고유한 값을 key으로 지정하게 되면, 해당 값만 추가되고 다른 아이템은 유지가 될 수 있다. 자세한 설명은 아래의 블로그가 자세히 설명해줬다.
+          // https://velopert.com/3636
+
+          // [ 그렇다면 react에서 추천하는 방법은 ]
+          // 1. 고유한 id 값을 넣도록 되어있다.
+          // 2. 또는 변수.toString()을 통해 메모리 값을 key값으로 쓰는것도 예제로 보여지고 있다.
+          // 3. react에서 추천한 블로그에서는 shortid 라이브러리를 활용하는 것을 최고 추천으로 하고 있다.
+          // https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318
+
+
+          // [ diff 알고리즘에 대해 알기 ]
+          // react는 vitural돔이라는 개념을 가지고 있다. 이를 통해 state가 변경 되었을 시 변경되 엘리먼트를 감지하고 변경되 영역만 실제 돔에 반영한다.
+          // 아마 다들 여기까지는 알고 있는 개념일 것이다. 근데 어떻게 변경시키는 걸까???
+
+          // diff 알고리즘이라고 하면 일반적으로 두 tree의 차이를 비교하는 알고리즘이라고 생각하면된다.
+          // 하지만 기존의 diff 알고리즘은 최악의 경우 O(N^3)의 시간복잡도가 나온다고 한다.
+          // 사실 왜 그런지에 대해서 찾아보고 싶었지만, 쉽게 찾을 순 없었다.일단 그정도 걸린다고만 알고있자
+
+          // 리엑트의 개선된 diff 알고리즘은 O(N) 까지 개선을 하였다. 핵심은 두가지이다.
+          // - 1. 일단 type을 기준으로 비교를 한다. 만약 다르다면 해당 지점부터의 트리를 모두 갱신한다.(unmount -> mount 시킨다.)
+          // - 2. 고유의 key가 존재한타면 key 매칭을 통해 비교를 한다.
+
+          // 위의 기준을 통해 diff 알고리즘이 이뤄진다.
+          // React의 JSX가 사실 이 virtual 돔과 연관이 있다.
+          
+          /*
+            [ 우리가 작성하는 jsx ]
+            <div className='cn'>
+              Content!
+            </div>
+
+            [ 실제 react에서 내부적으로 짜여지는 코드 ]
+            React.createElement(
+              'div',
+              { className: 'cn' },
+              'Content!',
+              'Content 2!'
+            );
+
+            [ 이를 통해 Virtual 돔 노드 구성 ]
+            {
+              type: 'div',
+              props: {
+                className: 'cn',
+                children: [
+                  'Content 1!',
+                  'Content 2!'
+                ]
+              }
+            }
+          */
+
+          // 일단 더 자세한 사항은 아래의 블로그를 참고하자
+          // https://www.holaxprogramming.com/2018/04/15/react-optimizing-virtual-dom-explained/
+          // https://reactjs.org/docs/introducing-jsx.html
+          // https://meetup.toast.com/posts/110
+          // https://www.cronj.com/blog/diff-algorithm-implemented-reactjs/
 
           // 성능상의 문제와 가독성의 문제로 <Try />로 컴포넌트로 나뉨
           tries.map((v, i) => (
             <Try key={`${i+v}차 시도`} tryInfo={v} index={i} />
           ))
+
         }
       </ul>
     </>
@@ -132,6 +209,7 @@ const NumberBaseball = () => {
 //         answer: getNumbers(),
 //         tries: [],
 //       });
+//       this.inputRef.current.focus();
 //     } else {
 //       const answerArr = value.split('').map((v) => parseInt(v));
 
@@ -147,6 +225,7 @@ const NumberBaseball = () => {
 //           answer: getNumbers(),
 //           tries: [],
 //         });
+//         this.inputRef.current.focus();
 //       } else {
 //         for (let i=0; i<4; i += 1) {
 //           if (answerArr[i] === answer[i]) {
@@ -159,6 +238,7 @@ const NumberBaseball = () => {
 //           tries: [...prevState.tries, { try: value, result: `${strike} 스트라이크, ${ball} 였습니다.` }],
 //           value: '',
 //         }));
+//         this.inputRef.current.focus();
 //       }
 //     }
 //   }
@@ -169,6 +249,7 @@ const NumberBaseball = () => {
 //       value: e.target.value,
 //     });
 //   }
+//   inputRef = createRef();
 
 //   render() {
 //     const { tries } = this.state;
@@ -177,7 +258,7 @@ const NumberBaseball = () => {
 //       <>
 //         <h1>{this.state.result}</h1>
 //         <form onSubmit={this.onSubmitForm}>
-//           <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
+//           <input ref={this.inputRef} maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
 //         </form>
 //         <div>시도: {tries.length}</div>
 //         <ul>
--- react/react_practice_4/src/Try.jsx
+++ react/react_practice_4/src/Try.jsx
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, memo } from 'react';
 
-const Try = ({ tryInfo }) => {
+// [ props와 state의 차이 이에 대한 대응 ]
+// props - 부모에게서 넘겨받은 값, 속성으로 property의 줄임말이다. props는 원칙상 자식에서 부모의 값을 변경하지 못하도록 되어있다.
+//      실제로 직접적으로 영향을 줄 순 없다. 그렇기 때문에, 만약 변경을 하고자 한다면, props를 변경시키는 함수를 부모컴포넌트에서 함께
+//      넘겨 준다. 이 함수를 자식 컴포넌트에서 호출시켜서 부모 컴포넌트에서 state를 변경하게끔 하고, 이를 다시 props로 받는 형태로 만들어 진다.
+
+// state - state는 말 그대로 상태 값이다. 각 컴포넌트가 자신이 보유하고 있는 값들을 가르킨다. 이 상태값을 통해 동적인 데이터 변경이 가능하게끔 한다.
+//    state의 변경은 this.setState()로 한다. 간혹 this.state = 로 값을 변경시키기도 한다. 하지만 이것은 실제 state가 변경된 것이 아니다.
+//    컴포넌트 내에 임시로 지정된 state를 변경시키는 형태이다. 즉 이를 relflesh하게 되면 휘발성 데이터로써 다시 원상 복귀하게 된다.
+//    즉 실제 state 변경은 this.setState()로만 한다.
+//
+//    [ this.setState()는 비동기식 ]
+//    this.setState()는 비동기식으로 작동한다. 왜일까? 이는 react는 내부적으로 this.setState() 를 통해 변경된 state를 모아뒀다가, 한꺼번에 batch() 하는
+//    형태로 변경되기 때문이다. 이를 통해 연산이 무분별하게 이뤄지는 것을 막고 연산 효율성을 증대시킨다.
+
+// [ props에 변경에 대한 최적화 ]
+// this.setState()를 하게 되면, react 컴포넌트 생명주기인 shouldUpdateComponent()를 타게 된다. 여기서 이전 state값과 새로운 state값을 비교하여, 다르면 내부적으로
+// true를 리턴시켜주고, true이면 render()가 실행된다.
+// 그렇다면 만약 값이 같을땐????
+// 실제로 실험을 해보면, 만약 shouldUpdateComponent()에 어떠한 설정을 하지 않았을 경우, 값이 동일 할 때도 render()을 타게 된다.
+// 기본적으로 이러한 경우 크게 문제가 될 일은 없다. 하지만 만약 부모 컴포넌트에서 props를 넘겨주는 상황이고, 이 props에 대한 render()가 호출 될때, 이는
+// 자식 컴포넌트들에게도 영향을 주게 되고, 만약 자식의 관계가 깊어질때 성능에 영향을 주게 될 것이다.
+// 이럴 때를 대비하여, 자식 컴포넌트 내부의 shouldUpdateComponent()에서 이전값과 새로운값에 대한 비교를 해주고 true 또는 false를 넘겨줘야한다.
+// https://velopert.com/3629
+
+// react Hooks는 shouldUpdateComponent() 또는 PureComponent 대신에
+// memo()라는 함수를 쓴다.
+const Try = memo(({ tryInfo }) => {
   return (
     <li>
       <div>{tryInfo.try}</div>
       <div>{tryInfo.result}</div>
     </li>
   );
-};
+});
 
 // class Try extends Component {
 //   render() {
